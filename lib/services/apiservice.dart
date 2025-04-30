33d6573f296e11f0292de10db64cfe946196607b
BounHiengSomphong <teng.ios123@gmail.com>
2025-04-30 09:10:16 +0700

    add api

--- lib/services/apiservice.dart
+++ lib/services/apiservice.dart
@@ -1,7 +1,82 @@
-class ApiConstants {
-  static const String baseUrl = 'http://100.75.106.34:3000'; //192.168.193.19
-  static const String loginEndpoint = '/api/auth/login';
-  static const String registerEndpoint = '/api/auth/register';
-  static const String user = '/';
-  // Add other endpoints as needed
+import 'dart:convert';
+import 'package:app_shoe/services/apiconstants.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ApiResponse {
+  final bool success;
+  final dynamic data;
+  final String? message;
+
+  ApiResponse({required this.success, this.data, this.message});
+}
+
+class ApiService {
+  Future<Map<String, String>> _buildHeaders() async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('token');
+
+    final headers = {'Content-Type': 'application/json'};
+
+    if (token != null && token.isNotEmpty) {
+      headers['Authorization'] = 'Bearer $token';
+    } else {
+      print('Token is null or empty');
+    }
+
+    return headers;
+  }
+
+  // ตัวอย่าง POST request
+  Future<ApiResponse> post(
+    String endpoint, {
+    required Map<String, dynamic> data,
+  }) async {
+    try {
+      final headers = await _buildHeaders();
+
+      final response = await http.post(
+        Uri.parse('${ApiConstants.baseUrl}$endpoint'),
+        headers: headers,
+        body: jsonEncode(data),
+      );
+
+      final responseBody = jsonDecode(response.body);
+
+      if (response.statusCode == 200) {
+        return ApiResponse(success: true, data: responseBody);
+      } else {
+        return ApiResponse(
+          success: false,
+          message: responseBody['message'] ?? 'Request failed',
+        );
+      }
+    } catch (e) {
+      return ApiResponse(success: false, message: 'Connection error');
+    }
+  }
+
+  Future<ApiResponse> get(String endpoint) async {
+    try {
+      final headers = await _buildHeaders();
+
+      final response = await http.get(
+        Uri.parse('${ApiConstants.baseUrl}$endpoint'),
+        headers: headers,
+      );
+
+      final responseBody = jsonDecode(response.body);
+
+      if (response.statusCode == 200) {
+        return ApiResponse(success: true, data: responseBody);
+      } else {
+        return ApiResponse(
+          success: false,
+          message: responseBody['message'] ?? 'Request failed',
+        );
+      }
+    } catch (e) {
+      return ApiResponse(success: false, message: 'Connection error');
+    }
+  }
 }
